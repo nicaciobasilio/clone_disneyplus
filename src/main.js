aff5ce36123934d535803d8ebbce0c5d993fe5b8
Nicacio Basilio <nicacioufrn@gmail.com>
2023-02-04 21:01:02 -0300

    construindo header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const BUTTONS = document.querySelectorAll('[data-tab-button]');
     const QUESTIONS = document.querySelectorAll('[data-faq-question]');
 
-    
+    const HEROSECTION = document.querySelector('.hero');
+    const ALTURAHERO = HEROSECTION.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const POSICAOATUAL = window.scrollY;
+
+        if (POSICAOATUAL < ALTURAHERO) {
+            ocultaElementosDoHeader;
+        } else {
+            exibeElementosDoHeader;
+        }
+    })
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < BUTTONS.length; i++) {
         BUTTONS[i].addEventListener('click', function(botao) {
             const ABAALVO = botao.target.dataset.tabButton;
@@ -14,11 +27,22 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Seção FAQ, Accordion
     for (let i = 0; i < QUESTIONS.length; i++) {
         QUESTIONS[i].addEventListener('click', abreOuFechaResposta);
         
     }
 })
+
+function ocultaElementosDoHeader() {
+    const HEADER = document.querySelector('.header');
+    HEADER.classList.add('header--is--hidden');
+}
+
+function exibeElementosDoHeader() {
+    const HEADER = document.querySelector('.header');
+    HEADER.classList.remove('header--is--hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const CLASSE ='faq__questions__item--is-open';
